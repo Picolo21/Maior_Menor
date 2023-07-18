c96b45cec1d93fb758a711a0610ed4d9328eaeaa
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-07-18 00:47:23 -0300

    refazendo o exercicio utilizando metodos, lista e expressoes lambda

--- Maior_Menor/Program.cs
+++ Maior_Menor/Program.cs
@@ -1,40 +1,44 @@
-﻿internal class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-        double value = 0;
-        double max = 0;
-        double min = 0;
+        List<double> list = new List<double>();
 
-        for (int i = 0; i <= 14; i++)
+        for (int i = 0; i < 15; i++)
         {
-            Console.Write($"Escreva o {i + 1}º número qualquer: ");
-            value = double.Parse(Console.ReadLine());
-            if ((max == 0) && (min == 0) && (i == 0))
+            try
             {
-                max = value;
-                min = value;
+                Console.Clear();
+                Console.Write($"Digite o {i + 1}º número: ");
+                double number = double.Parse(Console.ReadLine());
+                list.Add(number);
             }
-            
-            if ((max != 0) || (i > 0))
+            catch (Exception e)
             {
-                if (value > max)
-                {
-                    max = value;
-                }
-            }
-            
-            if ((min != 0) || (i > 0))
-            {
-                if (value < min)
-                {
-                    min = value;
-                }
+                Console.Clear();
+                Console.WriteLine(e.Message);
             }
         }
 
+        Console.Clear();
+        Console.WriteLine("Lista de números digitados:\n");
+        list.ForEach(number =>  Console.WriteLine($"{number}"));
         Console.WriteLine();
-        Console.WriteLine("O maior número é: " + max.ToString("F2"));
-        Console.WriteLine("O menor número é: " + min.ToString("F2"));
+
+        double maxNumber = CalculatesLargestNumber(list);
+        double minNumber = CalculatesSmallestNumber(list);
+
+        Console.WriteLine("O maior número é: " + maxNumber.ToString("F2"));
+        Console.WriteLine("O menor número é: " + minNumber.ToString("F2"));
+    }
+
+    private static double CalculatesLargestNumber(List<double> list)
+    {
+        return list.Max();
+    }
+
+    private static double CalculatesSmallestNumber(List<double> list)
+    {
+        return list.Min();
     }
 }
